f7fbd8555b905f2ec8b556342756235dc13ee9ef
Edouard Menayde <edouard.menayde@protonmail.com>
2020-04-12 12:17:14 +0200

    Up version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Iconer.MixProject do
   def project do
     [
       app: :iconer,
-      version: "0.1.0-alpha",
+      version: "0.1.1-alpha",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
